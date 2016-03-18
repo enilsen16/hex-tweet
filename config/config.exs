c4c89fdf2d672b9e204928e308e81881768d803a
Erik Nilsen <enilsen16@live.com>
2016-03-18 13:35:59 -0700

    add twitter api keys

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,10 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+ExTwitter.configure(
+   consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+   consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+   access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+   access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
+)
