d54369f525493a1177763e243010bde0e4d134d7
Erik Nilsen <enilsen16@live.com>
2016-03-18 13:41:32 -0700

    fix config

--- config/config.exs
+++ config/config.exs
@@ -29,9 +29,9 @@ use Mix.Config
 #
 #     import_config "#{Mix.env}.exs"
 
-ExTwitter.configure(
+config :extwitter, :oauth, [
    consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
    consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
    access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
    access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
-)
+]
