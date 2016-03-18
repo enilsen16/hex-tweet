5584cad9ffad13801f0441bde32b1118092a263d
Erik Nilsen <enilsen16@live.com>
2016-03-18 00:57:21 -0700

    HexTweet.sort

--- lib/hex_tweet/parse.ex
+++ lib/hex_tweet/parse.ex
@@ -1,11 +1,18 @@
 defmodule HexTweet.Parse do
   use HTTPoison.Base
 
+  defstruct [:name, :description, :url, :updated_at]
+
   def get(url) do
     HTTPoison.get(url)
   end
 
   def parse({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     Poison.Parser.parse!(body)
+  end
+
+  def sort(body) when is_map(body) do
+    [%HexTweet.Parse{name: body["meta"]["name"], description: body["meta"]["description"],
+    url: body[:url], updated_at: body["updated_at"]}]
   end
 end
--- test/parse_test.exs
+++ test/parse_test.exs
@@ -20,4 +20,13 @@ defmodule ParseTest do
       assert [%{}| _] = HexTweet.Parse.parse(response)
     end
   end
+
+  test "sorts correctly based on updated_at" do
+    use_cassette "httpoison_get" do
+      response = HexTweet.Parse.get("https://hex.pm/api/packages?sort=updated_at")
+                  |> HexTweet.Parse.parse
+      result = HexTweet.Parse.sort(List.first(response))
+      assert [%HexTweet.Parse{name: _, description: _, url: _, updated_at: _}] = result
+    end
+  end
 end
