067349efd3766dca366cc1a03e2e7fe03de68282
Erik Nilsen <enilsen16@live.com>
2016-03-26 19:53:42 -0700

    add a space between name and version number

--- lib/hex_tweet/tweet.ex
+++ lib/hex_tweet/tweet.ex
@@ -1,7 +1,7 @@
 defmodule HexTweet.Tweet do
 
   def build(structure) do
-    tweet = "#{structure.name}(#{structure.version}): #{structure.description} #{structure.url}"
+    tweet = "#{structure.name} (#{structure.version}): #{structure.description} #{structure.url}"
     case String.length(tweet) <= 158 do
       true ->
         {:ok, tweet}
@@ -16,10 +16,10 @@ defmodule HexTweet.Tweet do
   end
 
   defp _build(structure) do
-    length = String.length("#{structure.name}(#{structure.version}): \n#{structure.url}")
-    length_of_description = (158 - length) - 3
+    length = String.length("#{structure.name} (#{structure.version}): #{structure.url}")
+    length_of_description = (158 - length) - 4
     <<string::binary-size(length_of_description), _::binary >> = structure.description
     String.strip(string)
-    "#{structure.name}(#{structure.version}): #{string}... #{structure.url}"
+    "#{structure.name} (#{structure.version}): #{string}... #{structure.url}"
   end
 end
--- test/tweet_test.exs
+++ test/tweet_test.exs
@@ -7,14 +7,14 @@ defmodule TweetTest do
     result = HexTweet.Tweet.build(struct)
     assert {:ok, tweet} = result
     assert String.length(tweet) <= 158
-    assert tweet == "ExTwitter(0.6.3): Twitter client library for elixir. https://hex.pm/packages/extwitter"
+    assert tweet == "ExTwitter (0.6.3): Twitter client library for elixir. https://hex.pm/packages/extwitter"
   end
 
   test "truncates a tweet if it is greater than 160 chars" do
     struct = %HexTweet.Parse{name: "poolboy", description: "A hunky Erlang worker pool factory. This is an example application showcasing database connection pools using Poolboy and epgsql. ", url: "https://hex.pm/packages/poolboy", version: "1.5.1", updated_at: nil}
     result = HexTweet.Tweet.build(struct)
     assert {:ok, tweet} = result
-    assert tweet == "poolboy(1.5.1): A hunky Erlang worker pool factory. This is an example application showcasing database connection pools usi... https://hex.pm/packages/poolboy"
+    assert tweet == "poolboy (1.5.1): A hunky Erlang worker pool factory. This is an example application showcasing database connection pools us... https://hex.pm/packages/poolboy"
     assert String.length(tweet) <= 158
   end
 end
